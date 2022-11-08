1bd473ea44a77e3ed3ac2aeb8adc9385707ded59
JoseFreitas990 <freitasze990@gmail.com>
2022-11-08 17:31:56 +0000

    findGroupsByUserId

--- src/interfaces/group/group_repo.interface.ts
+++ src/interfaces/group/group_repo.interface.ts
@@ -4,6 +4,7 @@ import { Group } from '@prisma/client';
 export default interface IGroupRepository {
   findAllGroups(): Promise<Group[]>;
   findGroupById(id: string): Promise<Group>;
+  findGroupsByUserId(id: string): Promise<Group[]>;
   createGroup(group: CreateGroupDto): Promise<Group>;
   updateGroup(groupId: string, userData: CreateGroupDto): Promise<Number>;
   deleteGroup(groupId: string): Promise<Group>;
